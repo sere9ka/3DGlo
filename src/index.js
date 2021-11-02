da7e7afdf0bab146182e9d9781c0ee4a36a1b569
Sergey Kovylov <sere99ka@gmail.com>
2021-11-02 15:48:56 +0300

    добавил валидацию полей по заданию

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import timer from './modules/timer'
 import menu from './modules/menu'
 import modal from './modules/modal'
 import calculator from './modules/calc'
+import validator from './modules/validator'
 
 
 timer("08 november 2021")
 menu()
 modal()
 calculator()
+validator()
