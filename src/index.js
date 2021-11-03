581b71e6baeb252e4ff822f6357eb2242c93e6e1
Sergey Kovylov <sere99ka@gmail.com>
2021-11-03 18:11:12 +0300

    создал для слайдера модуль

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import modal from './modules/modal'
 import calculator from './modules/calc'
 import validator from './modules/validator'
 import tabs from './modules/tabs'
+import slider from './modules/slider'
 
 
 timer("08 november 2021")
@@ -12,3 +13,4 @@ modal()
 calculator()
 validator()
 tabs()
+slider()
